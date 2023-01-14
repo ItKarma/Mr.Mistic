94d57aac271567f20be34dce7df843f4b06c1a18
danzok <loppesofc@gmail.com>
2023-01-14 19:49:04 -0300

    add variables for dev,command in the testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('kjkk')
+import { Bot } from 'grammy';
+import 'dotenv/config';
+
+const bot = new Bot(String(process.env.TOKEN_BOT));
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+bot.start();
