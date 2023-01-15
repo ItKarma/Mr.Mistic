a721085f5939986eeab2213cf1c315c027d09de2
danzok <loppesofc@gmail.com>
2023-01-14 21:56:40 -0300

    add buttons inline

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import { Bot } from 'grammy';
+import { Bot, InlineKeyboard } from 'grammy';
 import 'dotenv/config';
 
 const bot = new Bot(String(process.env.TOKEN_BOT));
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+const inlineKeyboard = new InlineKeyboard().url(
+    "Read on TechCrunch",
+    "https://techcrunch.com/2016/04/11/this-is-the-htc-10/",
+);
+
+bot.command("start", (ctx) => {
+    ctx.reply("*Hi\\!* _Welcome_ to [grammY](https://grammy.dev)\\.",
+        { parse_mode: 'Markdown', reply_markup: inlineKeyboard })
+});
+
+
 
 bot.start();
